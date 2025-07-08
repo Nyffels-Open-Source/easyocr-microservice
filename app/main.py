26778d46896239f8741fbbfd63813e48de75444b
Chesney Nyffels <chesney@nyffels.be>
2025-07-08 11:22:08 +0200

    Revert "Bugfix for fergotten import"
    This reverts commit 7abce35095e08d1dc457f245df94b3aec47e34e5.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Query
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from pathlib import Path
